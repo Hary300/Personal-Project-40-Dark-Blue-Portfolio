7953a6345b473ceb9e0c2028ad951b1e75a8b393
Haryadi Nugraha Putra <h.nugraha.p@gmail.com>
2026-09-21 15:41:21 +0700

    feat: enhance footer component with social links and copyright information

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -1,7 +1,32 @@
+import { footerData } from '@/data/footerData';
+import Logo from '../shared/Logo';
+
 const Footer = () => {
+  const socials = footerData.socials;
   return (
-    <footer className='flex flex-col gap-6 md:flex-row justify-between items-center px-4 sm:px-10 lg:px-15 xl:px-30 py-10'>
-      Footer
+    <footer className='flex flex-col gap-6 md:flex-row md:justify-between md:items-center px-4 sm:px-10 lg:px-15 xl:px-30 py-10'>
+      <div className='flex flex-col gap-6'>
+        <Logo />
+        <p className='text-xs lg:text-sm text-neutral-400'>
+          {footerData.copyright}
+        </p>
+      </div>
+      <div className='flex gap-4'>
+        {socials.map((social) => {
+          const Icon = social.icon;
+          return (
+            <a
+              key={social.id}
+              href={social.href}
+              target='_blank'
+              rel='noopener noreferrer'
+              className='flex justify-center items-center size-10 rounded-full shrink-0 border'
+            >
+              <Icon />
+            </a>
+          );
+        })}
+      </div>
     </footer>
   );
 };
