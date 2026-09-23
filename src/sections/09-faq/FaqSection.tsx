6edabe32cfdcdab44b0b94219d78d7816d09f1ae
Haryadi Nugraha Putra <h.nugraha.p@gmail.com>
2026-09-23 13:46:28 +0700

    feat: add Accordion component to FaqSection for enhanced FAQ display

--- src/sections/09-faq/FaqSection.tsx
+++ src/sections/09-faq/FaqSection.tsx
@@ -2,11 +2,40 @@ import SectionWrapper from '@/components/layouts/SectionWrapper';
 import SectionTitle from '@/components/shared/SectionTitle';
 import { faqData } from '@/data/09-faqData';
 
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '@/components/ui/accordion';
+
 const FaqSection = () => {
   const header = faqData.header;
+  const faqs = faqData.faqs;
   return (
-    <SectionWrapper sectionId='faq' hasGap hasPy>
+    <SectionWrapper
+      sectionId='faq'
+      hasGap
+      hasPy
+      className='flex flex-col items-center'
+    >
       <SectionTitle title={header.title} subtitle={header.subtitle} />
+
+      <Accordion
+        type='single'
+        collapsible
+        defaultValue='item-1'
+        className='max-w-180 flex flex-col gap-5'
+      >
+        {faqs.map((item) => (
+          <AccordionItem key={item.value} value={item.value}>
+            <div className='bg-neutral-900 p-4 lg:p-6 rounded-2xl'>
+              <AccordionTrigger>{item.trigger}</AccordionTrigger>
+              <AccordionContent>{item.content}</AccordionContent>
+            </div>
+          </AccordionItem>
+        ))}
+      </Accordion>
     </SectionWrapper>
   );
 };
